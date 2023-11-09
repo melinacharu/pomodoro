73599f8e5afb0c9d601b9d4ce05815a90176ef03
Seth Holligan <sethdholligan@gmail.com>
2023-11-09 22:06:10 +0800

    helloworld function added

--- app.js
+++ app.js
@@ -77,3 +77,12 @@ function pause() {
     alert('are you sure? sit your lazy ass back down.')
     clearInterval(myInterval)
 }
+
+function deeznuts() {
+    alert('deez nuts are itchin')
+}
+
+
+function helloworld() {
+  
+}
